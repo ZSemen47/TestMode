b093a1e3db73ae4bd950c31f02e17d70d89ebf8d
Semen Zazulin <zazulinsemen@gmail.com>
2021-01-30 23:11:27 +0300

    Initial commit

---
+++ src/test/java/ru/netology/RegistrationInfo.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class RegistrationInfo {
+    private final String login;
+    private final String password;
+    private final String status;
+}
